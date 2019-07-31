74b4dacd3f2a8dcf01647f3ca44a2c8d939380e1
Ryuya Ikeda <rikeda71@gmail.com>
2019-07-31 18:07:43 +0900

    Add: trainer class

---
+++ sinra/layered_bilstm_crf/trainer.py
@@ -0,0 +1,74 @@
+from logging import getLogger, StreamHandler, INFO
+
+import torch
+import torch.nn as nn
+
+from .model import NestedNERModel
+from .dataset import NestedNERDataset
+from .char_encoder import BiLSTMEncoder
+
+logger = getLogger(name='NestedNERModel')
+handler = StreamHandler()
+handler.setLevel(INFO)
+logger.setLevel(INFO)
+logger.addHandler(handler)
+
+
+class Trainer:
+    def __init__(self, model: NestedNERModel, dataset: NestedNERDataset,
+                 lr: float = 1e-3, cg: float = 5.0,
+                 max_epoch: int = 50, batch_size: int = 64,
+                 optalgo: torch.optim.Optimizer = torch.optim.Adam):
+
+        self.device = torch.device(
+            'cuda' if torch.cuda.is_available() else 'cpu'
+        )
+        self.model: NestedNERModel = model.to(self.device)
+        self.dataset: NestedNERDataset = dataset
+        self.char_encoder = BiLSTMEncoder(
+            self.dataset.CHAR.vocab.vectors, 50, 50
+        )
+        self.cg = cg
+        self.epoch_size = max_epoch
+        self.batch_size = batch_size
+        self.optimizer = optalgo(self.model.parameters(), lr=lr)
+
+    def train(self):
+        for i in range(self.epoch_size):
+            all_loss = 0.0
+            iterator = self.dataset.get_batch(self.batch_size)
+            for j, data in enumerate(iterator):
+                batch_loss = 0
+                nested = 0  # this value show nested rank
+                next_step = True  # while this value is True, train a batch
+                # make mask
+                mask = data.label0 != 0
+                next_index = [[1] * int(torch.sum(word)) for word in mask]
+                mask = mask.float().to(self.device)
+                word = self.dataset.WORD.vocab.vectors[data.word].to(
+                    self.device)
+                char = None
+                # char = self.char_encoder(data.char, mask)
+                pos = self.dataset.POS.vocab.vectors[data.pos].to(self.device)
+                subpos = self.dataset.SUBPOS.vocab.vectors[data.subpos].to(
+                    self.device)
+                # TODO ここで，char embeddingをCNNやBiLSTMに突っ込んで，他のベクトルと形を一緒にする必要がある
+                input_embed = NestedNERModel.first_input_embedding(
+                    word, char, pos, subpos
+                )
+                while next_step and self.dataset.label_len > nested:
+                    labels = self.dataset.get_batch_true_label(data, nested)
+                    labels = self.model.shorten_label(labels, next_index)
+                    loss, next_step, predicted_labels, input_embed, next_index, mask \
+                        = self.model(input_embed, mask, labels, next_index)
+                    batch_loss += loss
+                    nested += 1
+                    print(nested)
+                    # TODO バッチ内の系列ごとにmerge_embeddingの長さが違うので，それらに対して，paddingを入れる必要がある
+
+                self.optimizer.zero_grad()
+                batch_loss.backward()
+                nn.utils.clip_grad_norm_(self.model.parameters(), self.cg)
+                self.optimizer.step()
+                all_loss += batch_loss
+            logger.info('epoch: {} loss: {}'.format(i + 1, all_loss))
---
+++ tests/test_trainer.py
@@ -0,0 +1,32 @@
+import unittest
+
+from sinra.layered_bilstm_crf.trainer import Trainer
+from sinra.layered_bilstm_crf.model import NestedNERModel
+from sinra.layered_bilstm_crf.dataset import NestedNERDataset
+
+
+class TrainerTest(unittest.TestCase):
+
+    def setUp(self):
+        self.label_to_id = {
+            '<pad>': 0,
+            'O': 1,
+            'B-PSN': 2,
+            'I-PSN': 3,
+            'B-LOC': 4,
+            'I-LOC': 5
+        }
+        self.dataset = NestedNERDataset(text_file_dir='tests/',
+                                        use_gpu=False,
+                                        word_min_freq=0)
+        dim = self.dataset.get_embedding_dim()
+        self.model = NestedNERModel(num_labels=6, dropout_rate=0.5,
+                                    word_emb_dim=dim['word'], char_emb_dim=0,
+                                    pos_emb_dim=dim['pos'],
+                                    id_to_label=self.label_to_id)
+        self.trainer = Trainer(
+            model=self.model, dataset=self.dataset, max_epoch=100, batch_size=4
+        )
+
+    def test_train(self):
+        self.trainer.train()
